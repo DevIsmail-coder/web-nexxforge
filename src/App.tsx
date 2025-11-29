64245b5c27b1789c725f30b356a8c0a58c3adedd
DevIsmail-coder <ismailolawale777@gmail.com>
2025-11-29 07:20:49 +0100

    updated the enroll page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LandPage from './pages/landing'
-import Signup from './Auth/signup'
 import Login from './Auth/login'
-import ForgotPassword from './Auth/forgot-password'
-import ResetPassword from './Auth/reset-password'
 import OutletPage from './global'
 import AboutUs from './pages/about'
 import Contact from './pages/contact'
@@ -17,17 +14,6 @@ function App() {
     {
       path: "/login",
       element: <Login />
-    },
-    {
-      path: "/signup",
-      element: <Signup />
-    },
-    {
-      path: "/forget-password",
-      element: <ForgotPassword />
-    }, {
-      path: "/reset-password",
-      element: <ResetPassword />
     },
     {
       path: "",
--- src/Auth/login.tsx
+++ src/Auth/login.tsx
@@ -3,7 +3,8 @@
 const Login = () => {
   return (
     <div>
-      login page
+
+welcome back
     </div>
   )
 }
